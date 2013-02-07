7cbe3891bc5a500833dc891b154608a3e13ebb9b
Jason Chen <jhchen7@gmail.com>
2013-02-06 19:58:24 -0800

    fix asset references

--- grunt.js
+++ grunt.js
@@ -43,7 +43,8 @@ module.exports = function(grunt) {
     },
     copy: {
       'bin/demo/scripts/dropkick.js': 'demo/scripts/dropkick.js',
-      'bin/demo/images/': 'demo/images/*.png'
+      'bin/demo/images/': 'demo/images/*.png',
+      'bin/tests/lib/': 'tests/lib/*'
     }
   });
 
