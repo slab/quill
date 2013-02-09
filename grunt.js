92d4e54ecf220668d1f090482da2eee35f3cf8be
Jason Chen <jhchen7@gmail.com>
2013-02-08 20:48:35 -0800

    bump version

--- grunt.js
+++ grunt.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     meta: {
-      version: '0.0.2',
+      version: '0.1.0',
       banner: 
         '/*! Stypi Editor - v<%= meta.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
         ' *  https://www.stypi.com/\n' +
--- lib/scribe-rails/version.rb
+++ lib/scribe-rails/version.rb
@@ -1,5 +1,5 @@
 module Scribe
   module Rails
-    VERSION = "0.0.2"
+    VERSION = "0.1.0"
   end
 end
