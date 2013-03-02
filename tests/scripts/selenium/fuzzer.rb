556a376cc9bdbb61099ebbdabc1e48af8a2eae07
Byron Milligan <byronm@cs.stanford.edu>
2013-03-01 19:26:29 -0800

    Remove scratch.

--- tests/scripts/selenium/fuzzer.rb
+++ tests/scripts/selenium/fuzzer.rb
@@ -44,31 +44,3 @@ NUM_EDITS.times do |i|
    adapter.op_to_selenium(random_edit)
    check_consistency(driver)
 end
-
-################################################################################
-# Scratch from experimenting with api
-################################################################################
-# font_size_button = driver.find_element(:class, "size")
-# puts "Font button: #{font_size_button}"
-# driver.action.move_to(font_size_button).click.perform
-# driver.switch_to.frame(driver.find_element(:tag_name, "iframe"))
-# editor = driver.find_element(:id, "scribe-container")
-# 
-# editor.click
-# editor.send_keys "Hello, scribe."
-# editor.send_keys " "
-# editor.send_keys "My name is Byron."
-# 10.times do editor.send_keys :arrow_left end
-# line = driver.find_element(:id, "line-63")
-# puts "Line: #{line}"
-# driver.action.click(line).perform
-# driver.action.move_to(line).click.perform
-# editor.send_keys "test test test"
-# driver.switch_to.default_content
-# font_size_button = driver.find_element(:class, "size")
-# puts "Font button: #{font_size_button}"
-# driver.action.move_to(font_size_button).click.perform
-# 
-# driver.switch_to.default_content
-# driver.action.move_by(10, 0).click.perform
-# driver.quit
