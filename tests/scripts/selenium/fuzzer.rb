2673efaa2288a0fd2408b2e4f105bc77bf07ad14
Byron Milligan <byronm@cs.stanford.edu>
2013-03-24 22:52:42 -0700

    Get a random delta instead of prev. custom format.

--- tests/scripts/selenium/fuzzer.rb
+++ tests/scripts/selenium/fuzzer.rb
@@ -8,12 +8,13 @@ ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 ################################################################################
 # Helpers for generating random edits
 ################################################################################
-def js_get_random_edit(driver)
-  return driver.execute_script "return parent.writer.getRandomOp();"
-end
-
-def js_get_random_delta(doc_delta, alphabet, num_edits)
-  return driver.execute_script "return window.DocGen.getRadomDelta", doc_delta, ALPHABET, 1)
+def js_get_random_delta(driver, doc_delta)
+  driver.switch_to.default_content
+  return driver.execute_script("return window.DeltaGen.getRandomDelta.apply(window.DeltaGen, arguments)",
+    doc_delta,
+    ALPHABET,
+    1)
+  driver.switch_to.frame(driver.find_element(:tag_name, "iframe"))
 end
 
 ################################################################################
@@ -49,7 +50,7 @@ adapter = SeleniumAdapter.new driver, writer
 ################################################################################
 doc_delta = js_get_doc_delta(driver)
 NUM_EDITS.times do |i|
-   delta = js_get_random_delta(doc_delta)
+   delta = js_get_random_delta(driver, doc_delta)
    puts i if i % 10 == 0
    adapter.op_to_selenium(random_delta)
    check_consistency(driver)
