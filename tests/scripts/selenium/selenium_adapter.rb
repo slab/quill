8fa03a66935d29cfcba70653ec445347ac9aaf51
Byron Milligan <byronm@cs.stanford.edu>
2013-04-11 13:10:44 -0700

    Fix indentation.

--- tests/scripts/selenium/selenium_adapter.rb
+++ tests/scripts/selenium/selenium_adapter.rb
@@ -139,10 +139,10 @@ class SeleniumAdapter
   end
 
   def click_button_from_toolbar(button_class)
-      @driver.switch_to.default_content
-      button = @driver.execute_script("return $('#editor-toolbar-writer > .#{button_class}')[0]")
-      button.click()
-      @driver.switch_to.frame(@driver.find_element(:tag_name, "iframe"))
+    @driver.switch_to.default_content
+    button = @driver.execute_script("return $('#editor-toolbar-writer > .#{button_class}')[0]")
+    button.click()
+    @driver.switch_to.frame(@driver.find_element(:tag_name, "iframe"))
   end
 
   def select_from_dropdown(dropdown_class, value)
