56ef031dcafcf67e560696c716d517e702a0b942
Jason Chen <jhchen7@gmail.com>
2013-06-12 19:24:00 -0700

    fix null error in firefox

--- vendor/assets/javascripts/rangy/rangy-core.js
+++ vendor/assets/javascripts/rangy/rangy-core.js
@@ -3287,7 +3287,7 @@ rangy.createModule("WrappedSelection", function(api, module) {
             if (implementsControlRange && implementsDocSelection && sel.docSelection.type == CONTROL) {
                 updateControlSelection(sel);
             } else {
-                sel._ranges.length = sel.rangeCount = sel.nativeSelection.rangeCount;
+                sel._ranges.length = sel.rangeCount = (sel.nativeSelection ? sel.nativeSelection.rangeCount : 0);
                 if (sel.rangeCount) {
                     for (var i = 0, len = sel.rangeCount; i < len; ++i) {
                         sel._ranges[i] = new api.WrappedRange(sel.nativeSelection.getRangeAt(i));
