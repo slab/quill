fc4b297537016371d6dde8cd98a16efd2859621d
Byron Milligan <byronm@cs.stanford.edu>
2013-07-11 11:36:00 -0700

    Added unit tests folder.

---
+++ tests/webdriver/unit/insert.rb
@@ -0,0 +1,18 @@
+# Set up webdriver
+# Set up editor
+# Make an insert test
+
+require "minitest/autorun"
+require "minitest/pride"
+require "minitest/reporters"
+MiniTest::Reporters.use!
+
+describe "Insert" do
+  it "should prepend to document" do
+    true
+  end
+
+  it "should append to document" do
+    true #raise "Error!"
+  end
+end
