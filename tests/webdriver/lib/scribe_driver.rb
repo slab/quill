4d0400ba953d30466a6da085246dded7ef19fe47
Byron Milligan <byronm@cs.stanford.edu>
2013-07-12 11:47:15 -0700

    Added helper to get doc length from ruby.

--- tests/webdriver/lib/scribe_driver.rb
+++ tests/webdriver/lib/scribe_driver.rb
@@ -1,4 +1,7 @@
 module ScribeDriver
+  #############################################################################
+  # JS Helpers
+  #############################################################################
   def execute_js(driver, src, args = nil)
     driver.switch_to.default_content
     result = driver.execute_script(src, *args)
@@ -6,6 +9,17 @@ module ScribeDriver
     return result
   end
 
+  def js_set_doc_delta(driver)
+    execute_js driver, "window.Fuzzer.docDelta = window.Fuzzer.cleanup(editor.getDelta());"
+  end
+
+  def js_get_doc_length(driver)
+    execute_js driver, "return window.editor.getLength()"
+  end
+
+  #############################################################################
+  # WebDriver helpers
+  #############################################################################
   def create_scribe_driver(browser, url)
     if browser == :firefox
       profile = Selenium::WebDriver::Firefox::Profile.new
