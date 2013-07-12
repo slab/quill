a2aa7a31a6da16d6b7046bfbe920b0ee2658eab4
byronm <byronm@cs.stanford.edu>
2013-07-11 22:42:18 -0700

    Added setup to run before each test.

--- tests/webdriver/unit/insert.rb
+++ tests/webdriver/unit/insert.rb
@@ -3,9 +3,16 @@ require "minitest/pride"
 require "minitest/reporters"
 MiniTest::Reporters.use!
 
-describe "Insert" do
-  it "should prepend to document" do
-    true
+
+describe "Test Insert" do
+  before do
+    editor_url = "file://#{File.join(File.expand_path(__FILE__),
+      '../../../..', 'build/tests/webdriver.html')}"
+    @driver = ScribeDriver.create_scribe_driver(:chrome, editor_url)
+    @editor = @driver.find_element(:class, "editor")
+    @adapter = WebdriverAdapter.new @driver, @editor
+    @adapter.focus()
+    ScribeDriver.js_set_doc_delta(@driver)
   end
 
   it "should append to document" do
