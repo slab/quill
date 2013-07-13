284080eef52942835bc69e2aab5b9654b8cc7e29
Byron Milligan <byronm@cs.stanford.edu>
2013-07-12 18:26:20 -0700

    First attempt at inserting at every index.

--- tests/webdriver/unit/insert.rb
+++ tests/webdriver/unit/insert.rb
@@ -12,12 +12,12 @@ describe "Test Insert" do
     @editor = @driver.find_element(:class, "editor")
     @adapter = WebdriverAdapter.new @driver, @editor
     @adapter.focus()
-    delta = { 'startLength' => 0,
-              'endLength' => 4,
-              'ops' => [{ 'value' => "abc\n"}]}
-    ScribeDriver.js_set_doc_delta(@driver, delta)
-    ScribeDriver.js_set_scribe_delta(@driver, delta)
-    @adapter.doc_length = 4
+    @start_delta = { 'startLength' => 0,
+                     'endLength' => 4,
+                     'ops' => [{ 'value' => "abc\n"}]}
+    ScribeDriver.js_set_doc_delta(@driver, @start_delta)
+    ScribeDriver.js_set_scribe_delta(@driver, @start_delta)
+    @adapter.doc_length = @start_delta['endLength']
   end
 
   after do
@@ -31,19 +31,29 @@ describe "Test Insert" do
     success.must_equal true
   end
 
-  it "should prepend to document" do
+  # Single character insertion at every position
+  it "should correctly insert a single character at every position" do
     doc_length = ScribeDriver.js_get_doc_length(@driver)
-    delta = { 'startLength' => doc_length,
-              'endLength' => doc_length + 1,
-              'ops' => [{ 'value' => "0"}, {'start' => 0, 'end' => doc_length }]}
-    apply_delta(delta)
+    (0...doc_length).each do |insert_at|
+      insert_at = 1
+      puts "Inserting at: #{insert_at}"
+      delta = ScribeDriver.make_insert_delta(@driver, doc_length, insert_at, 'a', {})
+      apply_delta(delta)
+      # Reset state for next iteration
+      ScribeDriver.js_set_doc_delta(@driver, @start_delta)
+      ScribeDriver.js_set_scribe_delta(@driver, @start_delta)
+      @adapter.cursor_pos = 0
+      @adapter.doc_length = @start_delta["endLength"]
+    end
   end
 
-  it "should append to document" do
-    doc_length = ScribeDriver.js_get_doc_length(@driver)
-    delta = { 'startLength' => doc_length,
-              'endLength' => doc_length + 1,
-              'ops' => [ {'start' => 0, 'end' => doc_length - 1, 'attributes' => {} }, { 'value' => "0"}, {'start' => doc_length - 1, 'end' => doc_length }]}
-    apply_delta(delta)
-  end
+  # it "should append to document" do
+  #   doc_length = ScribeDriver.js_get_doc_length(@driver)
+  #   delta = { 'startLength' => doc_length,
+  #             'endLength' => doc_length + 1,
+  #             'ops' => [ {'start' => 0, 'end' => doc_length - 1, 'attributes' => {} }, { 'value' => "0"}, {'start' => doc_length - 1, 'end' => doc_length }]}
+  #   apply_delta(delta)
+  # end
+
+  # Multiline insertion at every position
 end
