942e30a123dfb71a438f23c9a9bb99cb2252e072
Byron Milligan <byronm@cs.stanford.edu>
2013-07-15 15:27:39 -0700

    Fix undo/redo test.

--- tests/webdriver/unit/undo_redo.rb
+++ tests/webdriver/unit/undo_redo.rb
@@ -13,25 +13,23 @@ describe "Test undo redo" do
     @editor = @driver.find_element(:class, "editor")
     @adapter = WebdriverAdapter.new @driver, @editor
     @adapter.focus()
-    ScribeDriver.js_set_doc_delta(@driver)
+    ScribeDriver::JS.set_doc_delta(@driver)
   end
 
   after do
     @driver.quit
   end
 
-  # 1. Need a better utility for checking before/after state
-  # 2. Current delta_equals_editor is incorrect
   it "should undo/redo typing" do
     start_delta = { "startLength" => 0,
       "endLength" => 1, "ops" => [{ "value" => "\n" }] }
-    delta = { "startLength" => 1,
-      "endLength" => 4, "ops" => [{ "value" => "abc" }, {"start" => 0, "end" => 1}] }
-    @adapter.apply_delta delta
+    end_delta = { "startLength" => 0,
+      "endLength" => 4, "ops" => [{ "value" => "abc\n" }] }
+    @adapter.type_text "abc"
     @adapter.undo
-    assert ScribeDriver.delta_equals_editor(@driver, start_delta)
+    assert ScribeDriver::JS.editor_delta_equals start_delta
     @adapter.redo
-    # assert ScribeDriver.delta_equals_editor(@driver, delta)
+    assert ScribeDriver::JS.editor_delta_equals end_delta
   end
 
   # it "should undo redo deletes" do
