35342ed9e493417f2eff43f922bf332fc2fb04b5
Byron Milligan <byronm@cs.stanford.edu>
2013-07-15 19:16:59 -0700

    Added stubs for copy/paste.

---
+++ tests/webdriver/unit/copy_paste.rb
@@ -0,0 +1,37 @@
+require 'debugger'
+gem "minitest"
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/scribe_driver'
+require_relative '../lib/webdriver_adapter'
+
+describe "Test Copy Paste" do
+  before do
+    editor_url = "file://#{File.join(File.expand_path(__FILE__),
+      '../../../..', 'build/tests/webdriver.html')}"
+    @driver = ScribeDriver.create_scribe_driver(:chrome, editor_url)
+    ScribeDriver::JS.execute_js("window.Fuzzer.resetScribe()")
+    @editor = @driver.find_element(:class, "editor")
+    @adapter = WebdriverAdapter.new @driver, @editor
+    @adapter.focus()
+    ScribeDriver::JS.set_doc_delta
+    @adapter.doc_length = ScribeDriver::JS.get_doc_length
+  end
+
+  after do
+    @driver.quit
+  end
+
+  it "should copy and paste plain text" do
+    @adapter.type_text "abc"
+    @adapter.copy(0, 3)
+    # XXX: Adapter's paste doesn't work yet
+    @adapter.paste(3)
+  end
+
+  # it "should copy and paste newlines" do
+  # end
+
+  # it "should copy and paste formatted text" do
+  # end
+end
