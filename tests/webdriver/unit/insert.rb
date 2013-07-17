64eff8052698a650aec73ba72adc4962cf4ce156
Byron Milligan <byronm@cs.stanford.edu>
2013-07-17 14:43:59 -0700

    Test inserting \n at start of empty doc.

--- tests/webdriver/unit/insert.rb
+++ tests/webdriver/unit/insert.rb
@@ -28,6 +28,8 @@ describe "Test Insert" do
     success.must_equal true, err_msg
   end
 
+  # Test autoformatting: Inserting a character into any tiype
+  # Test newlines
   # Single character insertion at every position
   it "should insert a single character at every position" do
     doc_length = ScribeDriver::JS.get_doc_length
@@ -41,6 +43,20 @@ describe "Test Insert" do
       ScribeDriver::JS.set_doc_delta
       @adapter.doc_length = ScribeDriver::JS.get_doc_length
     end
+  end
+
+  it "should insert a newline in the empty document" do
+    start_delta = { "startLength" => 0,
+                    "endLength" => 1,
+                    "ops" => [{ "value" => "\n", "attributes" => {}}]}
+    ScribeDriver::JS.set_doc_delta start_delta
+    ScribeDriver::JS.set_scribe_delta start_delta
+    @adapter.doc_length = ScribeDriver::JS.get_doc_length
+    current_delta = { "startLength" => 1,
+                      "endLength" => 2,
+                      "ops" => [{ "value" => "\n", "attributes" => {} },
+                                { "start" => 0, "end" => 1}]}
+    apply_delta current_delta, "Inserting \n at start of empty document fails."
   end
 
   # Multiline insertion at every position
