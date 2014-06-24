0a8e2b87c462f42a0002acb74ae51335bae5aa2d
Jason Chen <jhchen7@gmail.com>
2014-06-23 17:10:31 -0700

    update sauce browser versions

--- test/karma.js
+++ test/karma.js
@@ -2,8 +2,8 @@ var _  = require('lodash');
 var os = require('os');
 
 
-var CHROME_VERSION = '34'
-var FIREFOX_VERSION = '28'
+var CHROME_VERSION = '35'
+var FIREFOX_VERSION = '30'
 var SAFARI_VERSION = '7'
 var IOS_VERSION = '7.1'
 var ANDROID_VERSION = '4.3'
