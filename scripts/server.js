432922515d9cd63066138865d23e4bb50a26e74f
Jason Chen <jhchen7@gmail.com>
2014-07-09 19:30:18 -0700

    fix theme build

--- scripts/server.js
+++ scripts/server.js
@@ -1,7 +1,9 @@
 var _ = require('lodash');
 var coffeeify = require('coffeeify');
 var connect = require('connect');
+var fs = require('fs');
 var harp = require('harp');
+var stylus = require('stylus');
 var watchify = require('watchify');
 
 var opts = { extensions: ['.js', '.coffee'] };
@@ -27,6 +29,18 @@ var respond = function(type, req, res, next) {
 
 app.use('/quill.js', respond.bind(this, 'main'));
 app.use('/test/quill.js', respond.bind(this, 'test'));
+app.use('/quill.snow.css', function(req, res, next) {
+  res.setHeader('Content-Type', 'text/css');
+  fs.readFile('./src/themes/snow/snow.styl', function(err, data) {
+    var s = stylus(data.toString());
+    s.include('./src/themes/snow');
+    s.define('url', stylus.url());
+    s.render(function(err, css) {
+      res.write(css);
+      res.end();
+    })
+  });
+});
 app.use(harp.mount(__dirname + '/..'));
 
 app.listen(9000);
