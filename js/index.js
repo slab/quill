3adca2d629c6cac2297436eaad1270eded27ddd6
Jason Chen <jhchen7@gmail.com>
2014-11-07 17:50:10 -0800

    log message inviting users to try out editor api

--- js/index.js
+++ js/index.js
@@ -24,4 +24,9 @@ $(document).ready(function() {
     $('.quill-wrapper').tooltip('destroy');
     clearTimeout(tooltipTimer);
   });
+
+  // Place our message is below Twitter's dpi warning
+  setTimeout(function() {
+    console.log("Welcome to Quill!\n\nThe editor on this page is available via `quill`. Give the API a try:\n\n\tquill.formatText(6, 10, 'bold', true);\n\nVisit the API documenation page to learn more: http://quilljs.com/docs/api/\n");
+  }, 1000)
 });
