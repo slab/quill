bef8496fbac20b4394c3a6f5198e0b0779ec5d5c
Jason Chen <jhchen7@gmail.com>
2015-10-08 21:02:02 -0700

    temporarily disable not working modules

--- examples/scripts/advanced.js
+++ examples/scripts/advanced.js
@@ -1,6 +1,6 @@
 var quillBasic = new Quill('.basic-wrapper .editor-container', {
   modules: {
-    authorship: { authorId: 'basic' },
+    // authorship: { authorId: 'basic' },
     toolbar: { container: '.basic-wrapper .toolbar-container' }
   },
   theme: false
@@ -8,27 +8,27 @@ var quillBasic = new Quill('.basic-wrapper .editor-container', {
 
 var quillAdvanced = new Quill('.advanced-wrapper .editor-container', {
   modules: {
-    'authorship': { authorId: 'advanced', enabled: true },
+    // 'authorship': { authorId: 'advanced', enabled: true },
     'toolbar': { container: '.advanced-wrapper .toolbar-container' },
-    'link-tooltip': true,
-    'image-tooltip': true,
-    'multi-cursor': true
+    // 'link-tooltip': true,
+    // 'image-tooltip': true,
+    // 'multi-cursor': true
   },
   styles: false,
   theme: 'snow'
 });
 
-var authorship = quillAdvanced.getModule('authorship');
-authorship.addAuthor('basic', 'rgba(255,153,51,0.4)');
+// var authorship = quillAdvanced.getModule('authorship');
+// authorship.addAuthor('basic', 'rgba(255,153,51,0.4)');
 
-var cursorManager = quillAdvanced.getModule('multi-cursor');
-cursorManager.setCursor('basic', 0, 'basic', 'rgba(255,153,51,0.9)');
+// var cursorManager = quillAdvanced.getModule('multi-cursor');
+// cursorManager.setCursor('basic', 0, 'basic', 'rgba(255,153,51,0.9)');
 
 quillBasic.on('selection-change', function(range) {
   console.info('basic', 'selection', range);
-  if (range != null) {
-    cursorManager.moveCursor('basic', range.end);
-  }
+  // if (range != null) {
+  //   cursorManager.moveCursor('basic', range.end);
+  // }
 });
 
 quillBasic.on('text-change', function(delta, source) {
