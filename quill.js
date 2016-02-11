6af2bd8b33cb7f60445517b3635bca4330658a22
Jason Chen <jhchen7@gmail.com>
2016-02-11 11:58:08 -0800

    fix imports

--- quill.js
+++ quill.js
@@ -1,18 +1,19 @@
 import Quill from './core/quill';
+import Parchment from 'parchment';
 
-import AlignFormat from './formats/align';
-import BackgroundFormat from './formats/background';
+import { AlignClass } from './formats/align';
+import { BackgroundClass } from './formats/background';
 import BoldFormat from './formats/bold';
 import CodeFormat from './formats/code';
-import ColorFormat from './formats/color';
-import DirectionFormat from './formats/direction';
-import FontFormat from './formats/font';
+import { ColorClass } from './formats/color';
+import { DirectionClass } from './formats/direction';
+import { FontClass } from './formats/font';
 import HeaderFormat from './formats/header';
-import IndentFormat from './formats/indent';
+import { IndentClass } from './formats/indent';
 import ItalicFormat from './formats/italic';
 import ListFormat from './formats/list';
 import ScriptFormat from './formats/script';
-import SizeFormat from './formats/size';
+import { SizeClass } from './formats/size';
 import StrikeFormat from './formats/strike';
 import UnderlineFormat from './formats/underline';
 
@@ -29,19 +30,19 @@ import ToolbarModule from './modules/toolbar';
 import SnowTheme from './themes/snow';
 
 
-Quill.register(AlignFormat);
-Quill.register(BackgroundFormat);
+Quill.register(AlignClass);
+Quill.register(BackgroundClass);
 Quill.register(BoldFormat);
 Quill.register(CodeFormat);
-Quill.register(ColorFormat);
-Quill.register(DirectionFormat);
-Quill.register(FontFormat);
+Quill.register(ColorClass);
+Quill.register(DirectionClass);
+Quill.register(FontClass);
 Quill.register(HeaderFormat);
-Quill.register(IndentFormat);
+Quill.register(IndentClass);
 Quill.register(ItalicFormat);
 Quill.register(ListFormat);
 Quill.register(ScriptFormat);
-Quill.register(SizeFormat);
+Quill.register(SizeClass);
 Quill.register(StrikeFormat);
 Quill.register(UnderlineFormat);
 
