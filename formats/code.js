e8068aef0ecd9d0f894519dd63981f445f4724c7
Jason Chen <jhchen7@gmail.com>
2016-03-14 23:17:00 -0700

    progress on vanilla code

--- formats/code.js
+++ formats/code.js
@@ -1,66 +1,44 @@
 import Delta from 'rich-text/lib/delta';
 import Parchment from 'parchment';
-import logger from '../core/logger';
 import Block from '../blots/block';
 import Inline from '../blots/inline';
 
 
-let debug = logger('quill:code');
-
-
-class Code extends Inline { }
+class Code extends Inline {
+  formats() {
+    return this.parent instanceof CodeBlock ? {} : super.formats();
+  }
+}
 Code.blotName = 'code';
 Code.tagName = 'CODE';
 
 
-class TokenAttributor extends Parchment.Attributor.Class {
-  value(domNode) {
-    return undefined;
-  }
-}
-
-let CodeToken = new TokenAttributor('token', 'hljs', {
-  scope: Parchment.Scope.INLINE
-});
-
-
 class CodeBlock extends Block {
-  format(name, value) {
-    // TODO allow changing language
-    debug.warn(`format(${name}, ${value}) called on code block`);
-  }
-
-  formatAt(index, length, name, value) {
-    debug.warn(`formatAt(${index}, ${length}, ${name}, ${value}) called on code block`);
-  }
-
-  formats() {
-    return {
-      'code-block': this.domNode.getAttribute('data-language') || true
-    };
-  }
-
-  getDelta() {
+  delta() {
     return this.domNode.innerText.split('\n').reduce((delta, text) => {
       return delta.insert(text).insert('\n', this.formats());
     }, new Delta());
   }
 
-  highlight() {
-    if (this.domNode.innerHTML === this.html) return;
-    this.domNode.innerHTML = this.domNode.innerText;
-    hljs.highlightBlock(this.domNode);
-    this.build();
-    this.html = this.domNode.innerHTML;
+  format(name, value) {
+    if (name !== this.statics.blotName) return;
+    super.formatAt(name, value);
   }
 
-  optimize(mutations) {
-    super.optimize(mutations);
-    this.highlight();
+  formatAt(index, length, name, value) {
+    if (name !== this.statics.blotName) return;
+    super.formatAt(index, length, name, value);
+  }
+
+  insertAt(index, value, def) {
+    if (def != null) return;  // Cannot insert embeds into code
+    let [child, offset] = this.children.find(index);
+    child.insertAt(offset, value);
   }
 }
 CodeBlock.blotName = 'code-block';
+CodeBlock.childless = 'code';
 CodeBlock.tagName = 'PRE';
 
 
-export { CodeToken, Code, CodeBlock as default };
+export { Code, CodeBlock as default };
