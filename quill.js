15a2e5dd7957819a19a095c9baca1ea12ac124d2
Jason Chen <jhchen7@gmail.com>
2016-03-14 22:33:21 -0700

    reorder

--- quill.js
+++ quill.js
@@ -1,23 +1,26 @@
 import Quill from './core';
 
 import { AlignClass as Align } from './formats/align';
-import { BackgroundStyle as Background } from './formats/background';
-import Blockquote from './formats/blockquote';
-import Bold from './formats/bold';
-import CodeBlock, { CodeInline } from './formats/code';
-import { ColorStyle as Color } from './formats/color';
 import { DirectionClass as Direction } from './formats/direction';
-import { FontClass as Font } from './formats/font';
-import Header from './formats/header';
 import { IndentClass as Indent } from './formats/indent';
-import Image from './formats/image';
+
+import Blockquote from './formats/blockquote';
+import Header from './formats/header';
+import List, { ListItem } from './formats/list';
+
+import { BackgroundStyle as Background } from './formats/background';
+import { ColorStyle as Color } from './formats/color';
+import { FontClass as Font } from './formats/font';
+import { SizeClass as Size } from './formats/size';
+
+import Bold from './formats/bold';
 import Italic from './formats/italic';
 import Link from './formats/link';
-import List, { ListItem } from './formats/list';
 import Script from './formats/script';
-import { SizeClass as Size } from './formats/size';
 import Strike from './formats/strike';
 import Underline from './formats/underline';
+
+import Image from './formats/image';
 import Video from './formats/video';
 
 import ImageTooltip from './modules/image-tooltip';
