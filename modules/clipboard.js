52bb3513d527176cf976070067a0c5a74aac7dc8
Jason Chen <jhchen7@gmail.com>
2016-03-18 00:22:58 -0700

    implement basic html paste coversion

--- modules/clipboard.js
+++ modules/clipboard.js
@@ -1,6 +1,51 @@
 import Delta from 'rich-text/lib/delta';
 import Emitter from '../core/emitter';
 import Module from '../core/module';
+import Parchment from 'parchment';
+
+
+function textSanitize(node, delta) {
+  if (node instanceof Text) {
+    let text = node.data.replace(/\s\s+/g, ' ');
+    delta.insert(text);
+  }
+  return delta;
+}
+
+function newlineSanitize(node, delta) {
+  if (['P', 'DIV', 'VIDEO', 'BR'].indexOf(node.tagName) > -1) {
+    delta.insert('\n');
+  }
+  return delta;
+}
+
+function blotSanitize(node, delta) {
+  let match = Parchment.query(node);
+  if (match == null) return delta;
+  if (match instanceof Parchment.Embed) {
+    let embed = {};
+    embed[match.blotName] = match.value(node);
+    delta.insert(embed, match.formats(node));
+  } else if (typeof match.formats === 'function') {
+    delta = delta.compose(new Delta().retain(delta.length(), match.formats(node)));
+  }
+  return delta;
+}
+
+let matchers = [
+  textSanitize,
+  newlineSanitize,
+  blotSanitize,
+];
+
+function sanitize(node) {  // Post-order traversal
+  return [].reduce.call(node.childNodes || [], function(delta, childNode) {
+    let childrenDelta = matchers.reduce(function(childrenDelta, matcher) {
+      return matcher(childNode, childrenDelta);
+    }, sanitize(childNode));
+    return delta.concat(childrenDelta);
+  }, new Delta());
+}
 
 
 class Clipboard extends Module {
@@ -17,7 +62,7 @@ class Clipboard extends Module {
     let range = this.quill.getSelection();
     if (range == null || range.length === 0 || e.defaultPrevented) return;
     let clipboard = e.clipboardData || window.clipboardData;
-    clipboard.setData('text', this.quill.getText(range))
+    clipboard.setData('text', this.quill.getText(range));
     if (e.clipboardData) {  // IE11 does not let us set non-text data
       clipboard.setData('application/json', JSON.stringify(this.quill.getContents(range)));
     }
@@ -46,7 +91,7 @@ class Clipboard extends Module {
     let intercept = (delta) => {
       this.container.focus();
       setTimeout(() => {
-        delta.insert(this.container.innerText);
+        delta = delta.concat(sanitize(this.container));
         this.container.innerHTML = '';
         callback(delta);
       }, 1);
