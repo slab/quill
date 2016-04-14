36180f6323171788d12520a4aab0120fb4bca5c5
Jason Chen <jhchen7@gmail.com>
2016-04-14 14:10:48 -0700

    polyfill endsWith

--- core/polyfill.js
+++ core/polyfill.js
@@ -17,3 +17,15 @@ if (!String.prototype.startsWith) {
     return this.substr(position, searchString.length) === searchString;
   };
 }
+
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+    var subjectString = this.toString();
+    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+      position = subjectString.length;
+    }
+    position -= searchString.length;
+    var lastIndex = subjectString.indexOf(searchString, position);
+    return lastIndex !== -1 && lastIndex === position;
+  };
+}
