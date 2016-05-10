1022f9b126007d230ca1a91fe924b1b80fed87e3
Jason Chen <jhchen7@gmail.com>
2016-05-10 00:11:24 -0700

    clean up release script

--- _develop/release.sh
+++ _develop/release.sh
@@ -4,17 +4,22 @@ rm -r .release
 rm -r dist
 mkdir .release
 mkdir .release/quill
+
 npm run build
 cp dist/quill.css dist/quill.bubble.css dist/quill.snow.css dist/quill.js .release/quill/
-printf "cdn: .\nversion: ." > .release/jekyll.yml
-jekyll build -s node_modules/quill-docs -d .release/_site --config node_modules/quill-docs/_config.yml,.release/jekyll.yml
-mkdir .release/quill/examples
-mv .release/_site/standalone/bubble/index.html .release/quill/examples/bubble.html
-mv .release/_site/standalone/snow/index.html .release/quill/examples/snow.html
-mv .release/_site/standalone/full/index.html .release/quill/examples/full.html
-find .release/quill/examples -type f -exec sed -i "" 's/\<link rel\="icon".*\>/ /g' {} \;
-find .release/quill/examples -type f -exec sed -i "" 's/href="\/\//href="https:\/\//g' {} \;
-find .release/quill/examples -type f -exec sed -i "" 's/src="\/\//src="https:\/\//g' {} \;
 uglifyjs .release/quill/quill.js > .release/quill/quill.min.js
+
 cd .release
+
+printf "cdn: .\nversion: ." > jekyll.yml
+jekyll build -s ../node_modules/quill-docs -d _site --config ../node_modules/quill-docs/_config.yml,jekyll.yml
+
+mkdir quill/examples
+mv _site/standalone/bubble/index.html quill/examples/bubble.html
+mv _site/standalone/snow/index.html quill/examples/snow.html
+mv _site/standalone/full/index.html quill/examples/full.html
+find quill/examples -type f -exec sed -i "" 's/\<link rel\="icon".*\>/ /g' {} \;
+find quill/examples -type f -exec sed -i "" 's/href="\/\//href="https:\/\//g' {} \;
+find quill/examples -type f -exec sed -i "" 's/src="\/\//src="https:\/\//g' {} \;
+
 tar -czf quill.tar.gz quill
