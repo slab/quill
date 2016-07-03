b779ae41d689cf099ae2a2223cb95396e7a842ef
Jason Chen <jhchen7@gmail.com>
2016-07-03 16:22:58 -0700

    fix #776

--- core/polyfill.js
+++ core/polyfill.js
@@ -31,26 +31,28 @@ if (!String.prototype.endsWith) {
 }
 
 if (!Array.prototype.find) {
-  Array.prototype.find = function(predicate) {
-    if (this === null) {
-      throw new TypeError('Array.prototype.find called on null or undefined');
-    }
-    if (typeof predicate !== 'function') {
-      throw new TypeError('predicate must be a function');
-    }
-    var list = Object(this);
-    var length = list.length >>> 0;
-    var thisArg = arguments[1];
-    var value;
-
-    for (var i = 0; i < length; i++) {
-      value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
+  Object.defineProperty(Array.prototype, "find", {
+    value: function(predicate) {
+      if (this === null) {
+        throw new TypeError('Array.prototype.find called on null or undefined');
       }
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+      var list = Object(this);
+      var length = list.length >>> 0;
+      var thisArg = arguments[1];
+      var value;
+
+      for (var i = 0; i < length; i++) {
+        value = list[i];
+        if (predicate.call(thisArg, value, i, list)) {
+          return value;
+        }
+      }
+      return undefined;
     }
-    return undefined;
-  };
+  });
 }
 
 // Disable resizing in Firefox
