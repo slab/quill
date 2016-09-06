6d3ca28b825f4ce41d5b65df4ea8eac7977deaf8
Ben Browitt <ben.browitt@gmail.com>
2016-09-06 23:28:35 +0300

    handle align and direction attributes on paste

--- formats/align.js
+++ formats/align.js
@@ -5,7 +5,8 @@ let config = {
   whitelist: ['right', 'center', 'justify']
 };
 
+let AlignAttribute = new Parchment.Attributor.Attribute('align', 'align', config);
 let AlignClass = new Parchment.Attributor.Class('align', 'ql-align', config);
 let AlignStyle = new Parchment.Attributor.Style('align', 'text-align', config);
 
-export { AlignClass, AlignStyle };
+export { AlignAttribute, AlignClass, AlignStyle };
