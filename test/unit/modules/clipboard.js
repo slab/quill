4f2ad7ddcecc7a73eb83a5ce3928bd9d89d10306
Jason Chen <jhchen7@gmail.com>
2016-09-15 16:00:50 -0700

    cleanup

--- test/unit/modules/clipboard.js
+++ test/unit/modules/clipboard.js
@@ -6,22 +6,13 @@ import Quill from '../../../core';
 describe('Clipboard', function() {
   describe('events', function() {
     beforeEach(function() {
-      this.event = {
-        clipboardData: {
-          getData: function() {},
-          setData: function() {}
-        },
-        preventDefault: function() {}
-      };
       this.quill = this.initialize(Quill, '<h1>0123</h1><p>5<em>67</em>8</p>');
       this.quill.setSelection(2, 5);
     });
 
     it('paste', function(done) {
-      this.event.clipboardData.types = ['text'];
-      spyOn(this.event.clipboardData, 'getData').and.returnValue('|');
       this.quill.clipboard.container.innerHTML = '<strong>|</strong>';
-      this.quill.clipboard.onPaste(this.event);
+      this.quill.clipboard.onPaste({});
       setTimeout(() => {
         expect(this.quill.root).toEqualHTML('<p>01<strong>|</strong><em>7</em>8</p>');
         expect(this.quill.getSelection()).toEqual(new Range(3));
