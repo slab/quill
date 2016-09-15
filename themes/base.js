341008cb5f1747998fe9bf60dd4735274f74bf0e
Jacob Müller <jacob.mueller.elz@gmail.com>
2016-09-15 03:02:24 +0200

    Use lowercase characters for all colors

--- themes/base.js
+++ themes/base.js
@@ -13,7 +13,7 @@ import icons from '../ui/icons';
 const ALIGNS = [ false, 'center', 'right', 'justify' ];
 
 const COLORS = [
-  "#000000", "#e60000", "#ff9900", "#ffff00", "#008A00", "#0066cc", "#9933ff",
+  "#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff",
   "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff",
   "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff",
   "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2",
