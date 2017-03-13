674ca7259b9e86b5b18b1eaf3aa8c28e6cdc55e0
Jason Chen <jhchen7@gmail.com>
2017-03-13 15:50:19 -0700

    dont require katex unless using formula

--- modules/formula.js
+++ modules/formula.js
@@ -1,5 +1,6 @@
 import Embed from '../blots/embed';
 import Quill from '../core/quill';
+import Module from '../core/module';
 
 
 class FormulaBlot extends Embed {
@@ -26,12 +27,16 @@ FormulaBlot.className = 'ql-formula';
 FormulaBlot.tagName = 'SPAN';
 
 
-class Formula {
+class Formula extends Module {
   static register() {
+    Quill.register(FormulaBlot, true);
+  }
+
+  constructor() {
+    super();
     if (window.katex == null) {
       throw new Error('Formula module requires KaTeX.');
     }
-    Quill.register(FormulaBlot, true);
   }
 }
 
