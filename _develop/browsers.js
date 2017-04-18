ad481a8294127e0e84843aebfe5966d348d7d67e
Jason Chen <jhchen7@gmail.com>
2017-04-17 19:54:30 -0700

    update platforms

--- _develop/browsers.js
+++ _develop/browsers.js
@@ -1,17 +1,17 @@
 var desktop = {
-  'mac-chrome-latest'       : ['OS X 10.11', 'chrome', '54.0'],
-  'mac-firefox-latest'      : ['OS X 10.11', 'firefox', '50.0'],
-  'mac-safari-latest'       : ['OS X 10.11', 'safari', '10.0'],
-  'mac-chrome-previous'     : ['OS X 10.10', 'chrome', '53.0'],
-  'mac-firefox-previous'    : ['OS X 10.10', 'firefox', '49.0'],
-  'mac-safari-previous'     : ['OS X 10.10', 'safari', '9.0'],
+  'mac-chrome-latest'       : ['OS X 10.12', 'chrome', '57.0'],
+  'mac-firefox-latest'      : ['OS X 10.12', 'firefox', '52.0'],
+  'mac-safari-latest'       : ['OS X 10.12', 'safari', '10.0'],
+  'mac-chrome-previous'     : ['OS X 10.11', 'chrome', '56.0'],
+  'mac-firefox-previous'    : ['OS X 10.11', 'firefox', '51.0'],
+  'mac-safari-previous'     : ['OS X 10.11', 'safari', '9.0'],
 
-  'windows-chrome-latest'   : ['Windows 10', 'chrome', '54.0'],
-  'windows-firefox-latest'  : ['Windows 10', 'firefox', '50.0'],
+  'windows-chrome-latest'   : ['Windows 10', 'chrome', '57.0'],
+  'windows-firefox-latest'  : ['Windows 10', 'firefox', '52.0'],
   'windows-edge-latest'     : ['Windows 10', 'microsoftedge', '14.14393'],
   'windows-ie-latest'       : ['Windows 8.1', 'internet explorer', '11.0'],
-  'windows-chrome-previous' : ['Windows 8.1', 'chrome', '53.0'],
-  'windows-firefox-previous': ['Windows 8.1', 'firefox', '49.0'],
+  'windows-chrome-previous' : ['Windows 8.1', 'chrome', '56.0'],
+  'windows-firefox-previous': ['Windows 8.1', 'firefox', '51.0'],
   'windows-edge-previous'   : ['Windows 10', 'microsoftedge', '13.10586'],
 
   'linux-chrome-latest'     : ['Linux', 'chrome', '48.0'],
@@ -24,8 +24,8 @@ var mobile = {
   'ios-latest'        : ['iPhone 7 Plus', 'iOS', '10.0', 'Safari'],
   'ios-previous'      : ['iPhone 6 Plus', 'iOS', '9.3', 'Safari'],
 
-  'android-latest'    : ['Android Emulator', 'Android', '5.1', 'Browser'],
-  'android-previous'  : ['Android Emulator', 'Android', '5.0', 'Browser']
+  'android-latest'    : ['Android Emulator', 'Android', '6.0', 'Browser'],
+  'android-previous'  : ['Android Emulator', 'Android', '5.1', 'Browser']
 };
 
 Object.keys(desktop).forEach(function(key) {
@@ -38,11 +38,10 @@ Object.keys(desktop).forEach(function(key) {
 });
 
 Object.keys(mobile).forEach(function(key) {
-  var appiumVersion = key.startsWith('ios') ? '1.6.3' : '1.5.3';
   module.exports[key] = {
     base: 'SauceLabs',
     browserName: mobile[key][3],
-    appiumVersion: appiumVersion,
+    appiumVersion: '1.6.3',
     deviceName: mobile[key][0],
     deviceOrientation: 'portrait',
     platformVersion: mobile[key][2],
