cf48ce8e5864ca0a99fad5ec85199387fd27d956
Erin Singer <erinsinger93@gmail.com>
2018-03-02 13:07:03 -0800

    Ensuring Quill picker is fully accessible
    Previously, the Quill picker was not a part of the tab order, which makes the picker inaccessible to screen readers/keyboard only users. Furthermore, once the picker was added to the tab order, I found that the picker options were not tabbable either, so I made the option items tabbable as well. I also made the dropdown escapable, so that screen reader/keyboard only users who opened the dropdown could exit out of it. Finally, I added the appropriate ARIA attributes to make sure screen readers could describe the state of the picker properly.

--- ui/picker.js
+++ ui/picker.js
@@ -1,5 +1,15 @@
 import DropdownIcon from '../assets/icons/dropdown.svg';
 
+const KEYCODES = {
+  ENTER: 13,
+  ESCAPE: 27,
+}
+
+let OPTIONS_ID = 0;
+
+function toggleAriaAttribute(element, attribute) {
+  element.setAttribute(attribute, !(element.getAttribute(attribute) === 'true'));
+}
 
 class Picker {
   constructor(select) {
@@ -8,14 +18,40 @@ class Picker {
     this.buildPicker();
     this.select.style.display = 'none';
     this.select.parentNode.insertBefore(this.container, this.select);
+
     this.label.addEventListener('mousedown', () => {
-      this.container.classList.toggle('ql-expanded');
+      this.togglePicker();
+    });
+    this.label.addEventListener('keydown', (event) => {
+      switch(event.keyCode) {
+        // Allows the "Enter" key to open the picker
+        case KEYCODES.ENTER:
+          this.togglePicker();
+          break;
+
+        // Allows the "Escape" key to close the picker
+        case KEYCODES.ESCAPE:
+          this.escape();
+          event.preventDefault();
+          break;
+        default:
+      }
     });
     this.select.addEventListener('change', this.update.bind(this));
   }
 
+  togglePicker() {
+    this.container.classList.toggle('ql-expanded');
+    // Toggle aria-expanded and aria-hidden to make the picker accessible
+    toggleAriaAttribute(this.label, 'aria-expanded');
+    toggleAriaAttribute(this.options, 'aria-hidden');
+  }
+
   buildItem(option) {
     let item = document.createElement('span');
+    item.tabIndex = '0';
+    item.setAttribute('role', 'button');
+
     item.classList.add('ql-picker-item');
     if (option.hasAttribute('value')) {
       item.setAttribute('data-value', option.getAttribute('value'));
@@ -26,6 +62,23 @@ class Picker {
     item.addEventListener('click', () => {
       this.selectItem(item, true);
     });
+    item.addEventListener('keydown', (event) => {
+      switch(event.keyCode) {
+        // Allows the "Enter" key to select an item
+        case KEYCODES.ENTER:
+          this.selectItem(item, true);
+          event.preventDefault();
+          break;
+
+        // Allows the "Escape" key to close the picker
+        case KEYCODES.ESCAPE:
+          this.escape();
+          event.preventDefault();
+          break;
+        default:
+      }
+    });
+
     return item;
   }
 
@@ -33,6 +86,9 @@ class Picker {
     let label = document.createElement('span');
     label.classList.add('ql-picker-label');
     label.innerHTML = DropdownIcon;
+    label.tabIndex = '0';
+    label.setAttribute('role', 'button');
+    label.setAttribute('aria-expanded', 'false');
     this.container.appendChild(label);
     return label;
   }
@@ -40,6 +96,18 @@ class Picker {
   buildOptions() {
     let options = document.createElement('span');
     options.classList.add('ql-picker-options');
+
+    // Don't want screen readers to read this until options are visible
+    options.setAttribute('aria-hidden', 'true');
+    options.tabIndex = '-1';
+
+    // Need a unique id for aria-controls
+    options.id = `ql-picker-options-${OPTIONS_ID}`;
+    OPTIONS_ID += 1;
+    this.label.setAttribute('aria-controls', options.id);
+
+    this.options = options;
+
     [].slice.call(this.select.options).forEach((option) => {
       let item = this.buildItem(option);
       options.appendChild(item);
@@ -59,8 +127,18 @@ class Picker {
     this.buildOptions();
   }
 
+  escape() {
+    // Close menu and return focus to trigger label
+    this.close();
+    // Need setTimeout for accessibility to ensure that the browser executes
+    // focus on the next process thread and after any DOM content changes
+    setTimeout(() => this.label.focus(), 1);
+  }
+
   close() {
     this.container.classList.remove('ql-expanded');
+    this.label.setAttribute('aria-expanded', 'false');
+    this.options.setAttribute('aria-hidden', 'true');
   }
 
   selectItem(item, trigger = false) {
