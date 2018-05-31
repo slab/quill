d259e5d8ca0b49095b5c6130f6673d576579562a
Jason Chen <jhchen7@gmail.com>
2018-05-31 15:32:48 -0700

    update platforms

--- _develop/browsers.js
+++ _develop/browsers.js
@@ -1,16 +1,16 @@
 const desktop = {
-  'mac-chrome-latest': ['OS X 10.13', 'chrome', '65.0'],
-  'mac-firefox-latest': ['OS X 10.13', 'firefox', '59.0'],
+  'mac-chrome-latest': ['OS X 10.13', 'chrome', '67.0'],
+  'mac-firefox-latest': ['OS X 10.13', 'firefox', '60.0'],
   'mac-safari-latest': ['OS X 10.13', 'safari', '11.0'],
-  'mac-chrome-previous': ['OS X 10.12', 'chrome', '64.0'],
-  'mac-firefox-previous': ['OS X 10.12', 'firefox', '58.0'],
+  'mac-chrome-previous': ['OS X 10.12', 'chrome', '66.0'],
+  'mac-firefox-previous': ['OS X 10.12', 'firefox', '59.0'],
   'mac-safari-previous': ['OS X 10.12', 'safari', '10.1'],
 
-  'windows-chrome-latest': ['Windows 10', 'chrome', '65.0'],
-  'windows-firefox-latest': ['Windows 10', 'firefox', '59.0'],
+  'windows-chrome-latest': ['Windows 10', 'chrome', '67.0'],
+  'windows-firefox-latest': ['Windows 10', 'firefox', '60.0'],
   'windows-edge-latest': ['Windows 10', 'microsoftedge', '16.16299'],
-  'windows-chrome-previous': ['Windows 8.1', 'chrome', '64.0'],
-  'windows-firefox-previous': ['Windows 8.1', 'firefox', '58.0'],
+  'windows-chrome-previous': ['Windows 8.1', 'chrome', '66.0'],
+  'windows-firefox-previous': ['Windows 8.1', 'firefox', '59.0'],
   'windows-edge-previous': ['Windows 10', 'microsoftedge', '15.15063'],
 };
 
@@ -40,7 +40,7 @@ Object.keys(mobile).forEach(key => {
   module.exports[key] = {
     base: 'SauceLabs',
     browserName: mobile[key][3],
-    appiumVersion: '1.7.2',
+    appiumVersion: '1.8.1',
     deviceName: mobile[key][0],
     deviceOrientation: 'portrait',
     platformVersion: mobile[key][2],
