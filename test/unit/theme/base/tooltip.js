9908711dfa52fbb58b4ae93efc75095a0dc7c05b
Jason Chen <jhchen7@gmail.com>
2018-06-02 17:00:19 -0700

    enforce prettier in linter, use default rule

--- test/unit/theme/base/tooltip.js
+++ test/unit/theme/base/tooltip.js
@@ -1,8 +1,7 @@
 import Quill from '../../../../core';
-import { BaseTooltip } from '../../../../themes/base'
+import { BaseTooltip } from '../../../../themes/base';
 
-class Tooltip extends BaseTooltip {
-}
+class Tooltip extends BaseTooltip {}
 
 Tooltip.TEMPLATE = '<input type="text">';
 
@@ -10,52 +9,70 @@ describe('BaseTooltip', function() {
   describe('save', function() {
     beforeEach(function() {
       this.quill = this.initialize(Quill, '');
-      this.tooltip = new Tooltip(this.quill)
+      this.tooltip = new Tooltip(this.quill);
     });
 
     it('converts youtube video url to embedded', function() {
       insertVideo(this.tooltip, 'http://youtube.com/watch?v=QHH3iSeDBLo');
-      expect(this.container.querySelector('.ql-video').src).toContain('http://www.youtube.com/embed/QHH3iSeDBLo');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'http://www.youtube.com/embed/QHH3iSeDBLo',
+      );
     });
 
     it('converts www.youtube video url to embedded', function() {
       insertVideo(this.tooltip, 'http://www.youtube.com/watch?v=QHH3iSeDBLo');
-      expect(this.container.querySelector('.ql-video').src).toContain('http://www.youtube.com/embed/QHH3iSeDBLo');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'http://www.youtube.com/embed/QHH3iSeDBLo',
+      );
     });
 
     it('converts m.youtube video url to embedded', function() {
       insertVideo(this.tooltip, 'http://m.youtube.com/watch?v=QHH3iSeDBLo');
-      expect(this.container.querySelector('.ql-video').src).toContain('http://www.youtube.com/embed/QHH3iSeDBLo');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'http://www.youtube.com/embed/QHH3iSeDBLo',
+      );
     });
 
     it('preserves youtube video url protocol', function() {
       insertVideo(this.tooltip, 'https://m.youtube.com/watch?v=QHH3iSeDBLo');
-      expect(this.container.querySelector('.ql-video').src).toContain('https://www.youtube.com/embed/QHH3iSeDBLo');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'https://www.youtube.com/embed/QHH3iSeDBLo',
+      );
     });
 
     it('uses https as default youtube video url protocol', function() {
       insertVideo(this.tooltip, 'youtube.com/watch?v=QHH3iSeDBLo');
-      expect(this.container.querySelector('.ql-video').src).toContain('https://www.youtube.com/embed/QHH3iSeDBLo');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'https://www.youtube.com/embed/QHH3iSeDBLo',
+      );
     });
 
     it('converts vimeo video url to embedded', function() {
       insertVideo(this.tooltip, 'http://vimeo.com/47762693');
-      expect(this.container.querySelector('.ql-video').src).toContain('http://player.vimeo.com/video/47762693/');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'http://player.vimeo.com/video/47762693/',
+      );
     });
 
     it('converts www.vimeo video url to embedded', function() {
       insertVideo(this.tooltip, 'http://www.vimeo.com/47762693');
-      expect(this.container.querySelector('.ql-video').src).toContain('http://player.vimeo.com/video/47762693/');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'http://player.vimeo.com/video/47762693/',
+      );
     });
 
     it('preserves vimeo video url protocol', function() {
       insertVideo(this.tooltip, 'https://www.vimeo.com/47762693');
-      expect(this.container.querySelector('.ql-video').src).toContain('https://player.vimeo.com/video/47762693/');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'https://player.vimeo.com/video/47762693/',
+      );
     });
 
     it('uses https as default vimeo video url protocol', function() {
       insertVideo(this.tooltip, 'vimeo.com/47762693');
-      expect(this.container.querySelector('.ql-video').src).toContain('https://player.vimeo.com/video/47762693/');
+      expect(this.container.querySelector('.ql-video').src).toContain(
+        'https://player.vimeo.com/video/47762693/',
+      );
     });
 
     function insertVideo(tooltip, url) {
