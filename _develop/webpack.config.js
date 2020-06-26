8a37869837df705bafa7cd9e52c73a9872aaba60
thisunravisara <thisunravisara@gmail.com>
2020-06-27 00:15:01 +0530

    Updated webpack to support chrome 58

--- _develop/webpack.config.js
+++ _develop/webpack.config.js
@@ -43,13 +43,13 @@ const jsRules = {
             {
               targets: {
                 browsers: [
-                  'last 2 Chrome major versions',
                   'last 2 Firefox major versions',
                   'last 2 Safari major versions',
                   'last 2 Edge major versions',
                   'last 2 iOS major versions',
                   'last 2 ChromeAndroid major versions',
                 ],
+                chrome: "58",
               },
             },
           ],
