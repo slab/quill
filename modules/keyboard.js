3d6d563e5748d077a8116562b93354c744f1a8bc
Lina Refai <linatrefai@yahoo.com>
2020-08-24 18:22:26 +0530

    ignoring new shortcut property based on keys

--- modules/keyboard.js
+++ modules/keyboard.js
@@ -25,6 +25,7 @@ class Keyboard extends Module {
   constructor(quill, options) {
     super(quill, options);
     this.bindings = {};
+    this.shortcuts = [];
     Object.keys(this.options.bindings).forEach(name => {
       if (this.options.bindings[name]) {
         this.addBinding(this.options.bindings[name]);
@@ -110,6 +111,12 @@ class Keyboard extends Module {
 
   listen() {
     this.quill.root.addEventListener('keydown', evt => {
+      if ( this.shortcuts.length ) {
+        for ( const sc of this.shortcuts ) {
+          const noMatch = Object.keys( sc ).find( key => evt[ key ] !== sc[ key ] )
+          if ( !noMatch ) return;
+        }
+      }
       if (evt.defaultPrevented || evt.isComposing) return;
       const bindings = (this.bindings[evt.key] || []).concat(
         this.bindings[evt.which] || [],
