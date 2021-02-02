bf721e6e307a8efd6351f32be79f0ed3fb3e427f
luin <i@zihua.li>
2021-02-03 00:54:23 +0800

    Cut across lines shouldn't affect the formatting of the line above
    To test:
    
    1. Open http://localhost:9000/standalone/full/
    
    2. Create two lines:
    
        ```
        Hea|der
        Normal |paragraph
        ```
    
    3. Cut text between two |.
    
    Expected:
    
    The first line is a header.
    
    Actually:
    
    The first line is a normal paragraph.

--- modules/clipboard.js
+++ modules/clipboard.js
@@ -19,6 +19,7 @@ import { ColorStyle } from '../formats/color';
 import { DirectionAttribute, DirectionStyle } from '../formats/direction';
 import { FontStyle } from '../formats/font';
 import { SizeStyle } from '../formats/size';
+import { deleteRange } from './keyboard';
 
 const debug = logger('quill:clipboard');
 
@@ -135,7 +136,7 @@ class Clipboard extends Module {
     e.clipboardData.setData('text/plain', text);
     e.clipboardData.setData('text/html', html);
     if (isCut) {
-      this.quill.deleteText(range, Quill.sources.USER);
+      deleteRange({ range, quill: this.quill });
     }
   }
 
