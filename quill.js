a7628785f6f6ab6d852b34f090e7b5dcb7509bd6
Alexander Khovansky <alex@khovansky.me>
2021-04-23 14:11:55 +0900

    Unregistered all quill formatting modules except the ones currently in use (bold, italic, underline) Made a sample file with all quill modules and cleaned up the live registration file

--- quill.js
+++ quill.js
@@ -1,109 +1,28 @@
 import Quill from './core';
 
-import { AlignClass, AlignStyle } from './formats/align';
-import {
-  DirectionAttribute,
-  DirectionClass,
-  DirectionStyle,
-} from './formats/direction';
-import Indent from './formats/indent';
-
-import Blockquote from './formats/blockquote';
-import Header from './formats/header';
-import List from './formats/list';
-
-import { BackgroundClass, BackgroundStyle } from './formats/background';
-import { ColorClass, ColorStyle } from './formats/color';
-import { FontClass, FontStyle } from './formats/font';
-import { SizeClass, SizeStyle } from './formats/size';
-
 import Bold from './formats/bold';
 import Italic from './formats/italic';
-import Link from './formats/link';
-import Script from './formats/script';
-import Strike from './formats/strike';
 import Underline from './formats/underline';
 
-import Formula from './formats/formula';
-import Image from './formats/image';
-import Video from './formats/video';
-
-import CodeBlock, { Code as InlineCode } from './formats/code';
-
-import Syntax from './modules/syntax';
-import Table from './modules/table';
-import Toolbar from './modules/toolbar';
-
-import Icons from './ui/icons';
-import Picker from './ui/picker';
-import ColorPicker from './ui/color-picker';
-import IconPicker from './ui/icon-picker';
-import Tooltip from './ui/tooltip';
-
-import BubbleTheme from './themes/bubble';
-import SnowTheme from './themes/snow';
+/**
+ * This file defines which formatting modules should be included
+ * into the final build of Quill.
+ * Currently only Bold, Italic and Underline are allowed
+ * Disabling modules in the `formats` setting of Quill options
+ * (client side) does not fully blocks their usage.
+ * For example, having background color module registered here will allow
+ * pasted content with the background color even if `formats` array
+ * does not have BG color formatting enabled.
+ *
+ * For the full list of modules originally included with Quill
+ * please see quill.reg.original.js
+ */
 
 Quill.register(
   {
-    'attributors/attribute/direction': DirectionAttribute,
-
-    'attributors/class/align': AlignClass,
-    'attributors/class/background': BackgroundClass,
-    'attributors/class/color': ColorClass,
-    'attributors/class/direction': DirectionClass,
-    'attributors/class/font': FontClass,
-    'attributors/class/size': SizeClass,
-
-    'attributors/style/align': AlignStyle,
-    'attributors/style/background': BackgroundStyle,
-    'attributors/style/color': ColorStyle,
-    'attributors/style/direction': DirectionStyle,
-    'attributors/style/font': FontStyle,
-    'attributors/style/size': SizeStyle,
-  },
-  true,
-);
-
-Quill.register(
-  {
-    'formats/align': AlignClass,
-    'formats/direction': DirectionClass,
-    'formats/indent': Indent,
-
-    'formats/background': BackgroundStyle,
-    'formats/color': ColorStyle,
-    'formats/font': FontClass,
-    'formats/size': SizeClass,
-
-    'formats/blockquote': Blockquote,
-    'formats/code-block': CodeBlock,
-    'formats/header': Header,
-    'formats/list': List,
-
     'formats/bold': Bold,
-    'formats/code': InlineCode,
     'formats/italic': Italic,
-    'formats/link': Link,
-    'formats/script': Script,
-    'formats/strike': Strike,
     'formats/underline': Underline,
-
-    'formats/formula': Formula,
-    'formats/image': Image,
-    'formats/video': Video,
-
-    'modules/syntax': Syntax,
-    'modules/table': Table,
-    'modules/toolbar': Toolbar,
-
-    'themes/bubble': BubbleTheme,
-    'themes/snow': SnowTheme,
-
-    'ui/icons': Icons,
-    'ui/picker': Picker,
-    'ui/icon-picker': IconPicker,
-    'ui/color-picker': ColorPicker,
-    'ui/tooltip': Tooltip,
   },
   true,
 );
