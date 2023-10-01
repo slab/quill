dc303a9d607e35b2e64c7001e8c0cce62c92f1ee
Zihua Li <i@zihua.li>
2023-10-01 08:42:41 +0800

    Add new module to handle ui node interactions

--- e2e/list.spec.ts
+++ e2e/list.spec.ts
@@ -2,22 +2,78 @@ import { expect } from '@playwright/test';
 import { test } from './fixtures';
 import { isMac } from './utils';
 
+const listTypes = ['bullet', 'ordered', 'checked'];
+
 test.describe('list', () => {
   test.beforeEach(async ({ editorPage }) => {
     await editorPage.open();
   });
 
-  test('navigating with shortcuts', async ({ page, editorPage }) => {
-    await editorPage.setContents([
-      { insert: 'item 1' },
-      { insert: '\n', attributes: { list: 'bullet' } },
-    ]);
+  for (const list of listTypes) {
+    test(`jump to line start (${list})`, async ({ page, editorPage }) => {
+      await editorPage.setContents([
+        { insert: 'item 1' },
+        { insert: '\n', attributes: { list } },
+      ]);
 
-    await editorPage.moveCursorAfterText('item 1');
-    await page.keyboard.press(isMac ? `Meta+ArrowLeft` : 'Home');
-    expect(await editorPage.getSelection()).toEqual({ index: 0, length: 0 });
+      await editorPage.moveCursorAfterText('item 1');
+      await page.keyboard.press(isMac ? `Meta+ArrowLeft` : 'Home');
+      expect(await editorPage.getSelection()).toEqual({ index: 0, length: 0 });
 
-    await page.keyboard.press(isMac ? `Meta+ArrowRight` : 'End');
-    expect(await editorPage.getSelection()).toEqual({ index: 6, length: 0 });
-  });
+      await page.keyboard.type('start ');
+      expect(await editorPage.getContents()).toEqual([
+        { insert: 'start item 1' },
+        { insert: '\n', attributes: { list } },
+      ]);
+    });
+
+    test.describe('navigation with left/right arrow keys', () => {
+      test(`move to previous/next line (${list})`, async ({
+        page,
+        editorPage,
+      }) => {
+        await editorPage.setContents([
+          { insert: 'first line' },
+          { insert: '\n', attributes: { list } },
+          { insert: 'second line' },
+          { insert: '\n', attributes: { list } },
+        ]);
+
+        await editorPage.moveCursorTo('s_econd');
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.press('ArrowLeft');
+        expect(await editorPage.getSelection()).toEqual({
+          index: 'first line'.length,
+          length: 0,
+        });
+        await page.keyboard.press('ArrowRight');
+        await page.keyboard.press('ArrowRight');
+        expect(await editorPage.getSelection()).toEqual({
+          index: 'first line\ns'.length,
+          length: 0,
+        });
+      });
+
+      test(`extend selection to previous/next line (${list})`, async ({
+        page,
+        editorPage,
+      }) => {
+        await editorPage.setContents([
+          { insert: 'first line' },
+          { insert: '\n', attributes: { list } },
+          { insert: 'second line' },
+          { insert: '\n', attributes: { list } },
+        ]);
+
+        await editorPage.moveCursorTo('s_econd');
+        await page.keyboard.press('Shift+ArrowLeft');
+        await page.keyboard.press('Shift+ArrowLeft');
+        await page.keyboard.type('a');
+        expect(await editorPage.getContents()).toEqual([
+          { insert: 'first lineaecond line' },
+          { insert: '\n', attributes: { list } },
+        ]);
+      });
+    });
+  }
 });
---
+++ modules/uiNode.ts
@@ -0,0 +1,98 @@
+import { ParentBlot } from 'parchment';
+import Module from '../core/module';
+import Quill from '../core/quill';
+
+const isMac = /Mac/i.test(navigator.platform);
+
+const canMoveCaretBeforeUINode = (event: KeyboardEvent) => {
+  if (
+    event.key === 'ArrowLeft' ||
+    event.key === 'ArrowRight' ||
+    event.key === 'ArrowUp' ||
+    event.key === 'ArrowDown' ||
+    event.key === 'Home'
+  ) {
+    return true;
+  }
+
+  if (isMac && event.key === 'a' && event.ctrlKey === true) {
+    return true;
+  }
+
+  return false;
+};
+
+class UINode extends Module {
+  isListening = false;
+  selectionChangeDeadline = 0;
+
+  constructor(quill: Quill, options: Record<string, never>) {
+    super(quill, options);
+
+    this.handleArrowKeys();
+    this.handleNavigationShortcuts();
+  }
+
+  private handleArrowKeys() {
+    this.quill.keyboard.addBinding({
+      key: 'ArrowLeft',
+      shiftKey: null,
+      handler(range, { line, offset, event }) {
+        if (offset === 0 && line instanceof ParentBlot && line.uiNode) {
+          this.quill.setSelection(
+            range.index - 1,
+            range.length + (event.shiftKey ? 1 : 0),
+            Quill.sources.USER,
+          );
+          return false;
+        }
+        return true;
+      },
+    });
+  }
+
+  private handleNavigationShortcuts() {
+    this.quill.root.addEventListener('keydown', (event) => {
+      if (!event.defaultPrevented && canMoveCaretBeforeUINode(event)) {
+        this.ensureListeningToSelectionChange();
+      }
+    });
+  }
+
+  private ensureListeningToSelectionChange() {
+    if (this.isListening) return;
+
+    this.isListening = true;
+    this.selectionChangeDeadline = Date.now() + 100;
+
+    const listener = () => {
+      this.isListening = false;
+
+      if (Date.now() <= this.selectionChangeDeadline) {
+        this.handleSelectionChange();
+      }
+    };
+
+    document.addEventListener('selectionchange', listener, {
+      once: true,
+    });
+  }
+
+  private handleSelectionChange() {
+    const selection = document.getSelection();
+    if (!selection) return;
+    const range = selection.getRangeAt(0);
+    if (range.collapsed !== true || range.startOffset !== 0) return;
+
+    const line = this.quill.scroll.find(range.startContainer);
+    if (!(line instanceof ParentBlot) || !line.uiNode) return;
+
+    const newRange = document.createRange();
+    newRange.setStartAfter(line.uiNode);
+    newRange.setEndAfter(line.uiNode);
+    selection.removeAllRanges();
+    selection.addRange(newRange);
+  }
+}
+
+export default UINode;
