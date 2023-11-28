62eb8ce3792bd31dce0b5f9e8eb8d62fbc19a034
Zihua Li <i@zihua.li>
2023-11-28 12:15:48 +0800

    Remove unused npm scripts for quill

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,7 +15,7 @@ mkdir .release
 mkdir .release/quill
 
 npm run build
-npx webpack --config _develop/webpack.config.js --env minimize
+npx webpack --env minimize
 cp dist/quill.core.css dist/quill.bubble.css dist/quill.snow.css dist/quill.js dist/quill.core.js dist/quill.min.js dist/quill.min.js.map .release/quill/
 
 cd .release
