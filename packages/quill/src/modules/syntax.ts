3536d8220e2285e571f24be012ca52e90eb2600e
Zihua Li <i@zihua.li>
2024-01-17 00:44:23 +0800

    Remove unnecessary ts comments

--- packages/quill/src/modules/syntax.ts
+++ packages/quill/src/modules/syntax.ts
@@ -30,7 +30,6 @@ class CodeToken extends Inline {
   constructor(scroll: ScrollBlot, domNode: Node, value: unknown) {
     // @ts-expect-error
     super(scroll, domNode, value);
-    // @ts-expect-error
     TokenAttributor.add(this.domNode, value);
   }
 
@@ -38,7 +37,6 @@ class CodeToken extends Inline {
     if (format !== CodeToken.blotName) {
       super.format(format, value);
     } else if (value) {
-      // @ts-expect-error
       TokenAttributor.add(this.domNode, value);
     } else {
       TokenAttributor.remove(this.domNode);
