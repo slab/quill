a9d933b72a1ad10bf46302da3daf3aa96f990ab4
fnlCtrl <fnlctrl@gmail.com>
2024-02-14 17:53:36 -0800

    preserve esmodule for babel

--- packages/quill/babel.config.js
+++ packages/quill/babel.config.js
@@ -1,7 +1,10 @@
 const pkg = require('./package.json');
 
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    '@babel/preset-typescript'
+  ],
   plugins: [
     ['transform-define', { QUILL_VERSION: pkg.version }],
     './scripts/babel-svg-inline-import',
