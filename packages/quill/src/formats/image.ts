89db879fa0b8de4fc4aade2a2c71fe47993be335
BURNING <102145525+burningxxx@users.noreply.github.com>
2024-04-12 23:19:45 +0700

    Save style attribute to keep image positioning

--- packages/quill/src/formats/image.ts
+++ packages/quill/src/formats/image.ts
@@ -1,7 +1,7 @@
 import { EmbedBlot } from 'parchment';
 import { sanitize } from './link.js';
 
-const ATTRIBUTES = ['alt', 'height', 'width'];
+const ATTRIBUTES = ['alt', 'height', 'width', 'style'];
 
 class Image extends EmbedBlot {
   static blotName = 'image';
