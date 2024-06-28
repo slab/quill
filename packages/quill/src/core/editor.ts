c3ea6788b97f0457491eb29c1640c6eddd5ac824
david ruty <david.ruty@arche-mc2.fr>
2024-06-28 09:44:33 +0200

    pretier

--- packages/quill/src/core/editor.ts
+++ packages/quill/src/core/editor.ts
@@ -338,10 +338,10 @@ function convertListHTML(
   const [{ child, offset, length, indent, type }, ...rest] = items;
   const [tag, attribute] = getListType(type);
   // CLASSNAME FIX
-  const className = 
+  const className =
     (<HTMLLIElement>child.domNode).className != ''
       ? ` class="${(<HTMLLIElement>child.domNode).className}"`
-      : "";
+      : '';
   if (indent > lastIndent) {
     types.push(type);
     if (indent === lastIndent + 1) {
