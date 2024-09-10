40913224e2139ba253e722fe4809b469477febb2
ropotv <ropotvs@gmail.com>
2024-09-10 14:07:33 +0300

    update policy name

--- packages/quill/src/core/utils/createTrustedHtml.ts
+++ packages/quill/src/core/utils/createTrustedHtml.ts
@@ -1,6 +1,6 @@
 const createTrustedHtml = (html: string): string => {
   if (window.trustedTypes && window.trustedTypes.createPolicy) {
-    const policy = window.trustedTypes.createPolicy('quill.js', {
+    const policy = window.trustedTypes.createPolicy('quill', {
       createHTML: (val) => val,
     });
     return policy.createHTML(html) as unknown as string;
