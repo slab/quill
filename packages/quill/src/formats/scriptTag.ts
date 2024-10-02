b91c347d5a6e55b1f12b601b37910b2f855d591d
hoavm <hoavm@avadagroup.com>
2024-10-02 10:22:41 +0700

    minor update

--- packages/quill/src/formats/scriptTag.ts
+++ packages/quill/src/formats/scriptTag.ts
@@ -3,26 +3,6 @@ import Block from '../blots/block.js';
 class ScriptTag extends Block {
   static blotName = 'script';
   static tagName = 'SCRIPT';
-
-  static insertAt() {
-    return;
-  }
-
-  static insertBefore() {
-    return;
-  }
-
-  static replaceWith() {
-    return;
-  }
-
-  format() {
-    return;
-  }
-
-  formatAt() {
-    return;
-  }
 }
 
 export default ScriptTag;
